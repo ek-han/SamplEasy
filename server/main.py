06c1b54c33de2bd1bcd43c65f01413e3523b1ed4
chalory <chantalrain.pino@gmail.com>
2022-07-02 23:15:23 +0800

    chore: get reccomendations

--- server/main.py
+++ server/main.py
@@ -1,21 +1,46 @@
 from fastapi import FastAPI
 import httpx
-import spotipy
-from spotipy.oauth2 import SpotifyOAuth
+from urllib import request
+import requests
+from requests.structures import CaseInsensitiveDict
+import json
 
+# max_popularity
+# min_popularity
 
 
 app = FastAPI()
 
-sp = spotipy.Spotify(auth_manager=SpotifyOAuth(client_id="28ffcff09ef14b88883d00d860f4c680",
-                                               client_secret="9e206a28d9934fd9812251d25de963f5",
-                                               redirect_uri="http://localhost:3000/callback",
-                                               scope="user-library-read"))
+genre = "classical"
+target_key = 1 # 0 - 11
+popularity = 30 # / 100
+tempo = 150 # beats per minute
 
-@app.get("/")
-def get_categories():
-    top_artists = sp.current_user_followed_artists(limit=20, after=None)
-    print(top_artists)
+@app.get("/get_categories")
+def get_recommendations(genre, target_key, popularity, tempo):
+    type = "track"
+    
+    # url given parameters
+    url = f"https://api.spotify.com/v1/recommendations?seed_genres={genre}&target_key={target_key}&target_popularity={popularity}&target_tempo={tempo}&type={type}"
+
+    headers = CaseInsensitiveDict()
+
+    token = 'BQCgRqyY2bN4aHKAPwqkvrB3HMHId4M7eM_9nprphvxyNGUL3fhfU5NkivgD_T5rPRFV6dIADDj1GwBgKWwjxVSUPvO3DahHf6ZM7Q2uZ8yGGkpLtSfFMFCxstgdlw7F9n9rk9aKHpUbg7jhuXf-kFr_of2rgAKNxjWtnxbd4CjU-EfhFbKa865K9JI'
+    headers ["Accept"] = "application/json"
+    headers ["Authorization"]= f"Bearer {token}"
 
 
-get_categories()
+    response = requests.get(url = url,headers=headers)
+    # d = json.loads(response.text)
+
+
+    print(response.status_code)
+    # print("hi")
+    # d['tracks'][idx] -> ['album', 'artists', 'available_markets', 'disc_number', 'duration_ms', 'explicit', 'external_ids', 'external_urls', 'href', 'id', 'is_local', 'name', 'popularity', 'preview_url', 'track_number', 'type', 'uri']
+    # 
+    # print(d['tracks'][6]['href'])
+    print(response.text)
+    
+    
+   
+get_recommendations(genre, target_key, popularity, tempo)
