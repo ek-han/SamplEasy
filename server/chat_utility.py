7e09e79b70eef2b39709737fb1a98a8bd624265c
chalory <chantalrain.pino@gmail.com>
2022-07-03 08:09:54 +0800

    websocket chat

--- server/chat_utility.py
+++ server/chat_utility.py
@@ -1,14 +1,14 @@
-import pymongo
+# import pymongo
 import datetime
 from fastapi import FastAPI
 # app = FastAPI()
 
 url = "mongodb+srv://Cherlord1:Cherlord1@cluster0.2img3.mongodb.net/?retryWrites=true&w=majority"
-client = pymongo.MongoClient(url)
-db = client["Database"]
+# client = pymongo.MongoClient(url)
+# db = client["Database"]
 
-acc_coll = db["Accounts"]
-msg_coll = db["Messages"]
+# acc_coll = db["Accounts"]
+# msg_coll = db["Messages"]
 
 # fields for accounts
 username = "username"
@@ -21,84 +21,84 @@ conversation_key = "conversation_key"
 created_at = "created_at"
 
 # for initial tests
-def initDummyMesages():
-    for msg in "1234":
-            str = "ab"
-            addMessage(msg, str[0], str[1])
-    for msg in "5678":
-            str = "ab"
-            addMessage(msg, str[1], str[0])
+# def initDummyMesages():
+#     for msg in "1234":
+#             str = "ab"
+#             addMessage(msg, str[0], str[1])
+#     for msg in "5678":
+#             str = "ab"
+#             addMessage(msg, str[1], str[0])
     
-def getAllMessages():
-    docs = msg_coll.find({})
-    for doc in docs: print(doc)
+# def getAllMessages():
+#     docs = msg_coll.find({})
+#     for doc in docs: print(doc)
 
-def maybeForDemo():
-    msg_coll.delete_many({})
-    addMessage("Hi there!", "Eric", "Lily")
-    addMessage("Hi there!", "Eric", "Lily")
-    addMessage("Hi there!", "Eric", "Lily")
-    addMessage("Hi there!", "Eric", "Lily")
-    addMessage("Hi there!", "Eric", "Lily")
-    addMessage("Hi there!", "Eric", "Lily")
+# def maybeForDemo():
+#     msg_coll.delete_many({})
+#     addMessage("Hi there!", "Eric", "Lily")
+#     addMessage("Hi there!", "Eric", "Lily")
+#     addMessage("Hi there!", "Eric", "Lily")
+#     addMessage("Hi there!", "Eric", "Lily")
+#     addMessage("Hi there!", "Eric", "Lily")
+#     addMessage("Hi there!", "Eric", "Lily")
     
-def returnAllMessages():
-    docs = msg_coll.find({})
-    return docs
+# def returnAllMessages():
+#     docs = msg_coll.find({})
+#     return docs
 
-# utility methods 
-# @app.post("/send_message")
-# def addMessage(msg, from_user, to_user):
-#     key = f"{from_user}{to_user}" if from_user < to_user else f"{to_user}{from_user}"
+# # utility methods 
+# # @app.post("/send_message")
+# # def addMessage(msg, from_user, to_user):
+# #     key = f"{from_user}{to_user}" if from_user < to_user else f"{to_user}{from_user}"
     
-#     doc = {
-#         message : msg,
-#         from_user_key : from_user,
-#         to_user_key : to_user,
-#         conversation_key : key,
-#         created_at : datetime.datetime.utcnow()
-#     }
+# #     doc = {
+# #         message : msg,
+# #         from_user_key : from_user,
+# #         to_user_key : to_user,
+# #         conversation_key : key,
+# #         created_at : datetime.datetime.utcnow()
+# #     }
     
-#     msg_coll.insert_one(doc)
+# #     msg_coll.insert_one(doc)
     
-def getMessages(current_user):
-    pipeline = [
-        # messages involving current user
-        {
-            "$match" : {
-                "$or" : [
-                    { from_user_key : current_user },
-                    { to_user_key : current_user }
-                ]
-            }
-        },
-        # grouped by conservation key
-        {
-            "$group" : {
-                "_id" : f'${conversation_key}'
-            }
-        },
-        # sort by most recent
-        {
-            "$sort" : {
-                created_at : -1 
-            }
-        }
-    ]
+# def getMessages(current_user):
+#     pipeline = [
+#         # messages involving current user
+#         {
+#             "$match" : {
+#                 "$or" : [
+#                     { from_user_key : current_user },
+#                     { to_user_key : current_user }
+#                 ]
+#             }
+#         },
+#         # grouped by conservation key
+#         {
+#             "$group" : {
+#                 "_id" : f'${conversation_key}'
+#             }
+#         },
+#         # sort by most recent
+#         {
+#             "$sort" : {
+#                 created_at : -1 
+#             }
+#         }
+#     ]
     
-    docs = msg_coll.aggregate(
-        pipeline
-    )
+#     docs = msg_coll.aggregate(
+#         pipeline
+#     )
     
-    print(list(docs))
+#     print(list(docs))
     
-    # for doc in docs: 
-    #     for a in doc:
-    #         print(a)
-    print("jhio")
+#     # for doc in docs: 
+#     #     for a in doc:
+#     #         print(a)
+#     print("jhio")
     
-# msg_coll.delete_many({})
-# msg_coll.insert_one({"msg":"hi there"})
-# initDummyMesages()
-# getAllMessages()
-# getMessages("a")
+# # msg_coll.delete_many({})
+# # msg_coll.insert_one({"msg":"hi there"})
+# # initDummyMesages()
+# # getAllMessages()
+# # getMessages("a")
--- server/main.py
+++ server/main.py
@@ -1,18 +1,23 @@
 from lib2to3.pgen2 import token
-from fastapi import FastAPI
+from fastapi import (
+    FastAPI, WebSocket, WebSocketDisconnect, Request, Response)
+from typing import List
 # import httpx
 from urllib import request
 import requests
 from requests.structures import CaseInsensitiveDict
 import json
-import uvicorn
 from chat_utility import *
+from typing import List
 from pydantic import BaseModel
+from fastapi.templating import Jinja2Templates
+
 
 
 from fastapi.middleware.cors import CORSMiddleware
 
 app = FastAPI()
+
 
 origins = [
     "http://localhost",
@@ -35,26 +40,93 @@ target_key = 1 # 0 - 11
 popularity = 30 # / 100
 tempo = 150 # beats per minute
 
-class Model(BaseModel):
-    msg: str
+templates = Jinja2Templates(directory="templates")
 
-@app.post("/send_message")
-def addMessage(data: Model):
-    
-    doc = {
-        message : data.msg,
-        created_at : datetime.datetime.utcnow()
-    }
-    
-    msg_coll.insert_one(doc)
-    
-    print('hello')
-    return 'world'
 
-@app.get("/getAllMessages")
-def getAllMessages():
-    docs = msg_coll.find({})
-    return docs
+@app.get("/")
+def get_home(request: Request):
+    return templates.TemplateResponse("home.html", {"request": request})
+
+
+@app.get("/chat")
+def get_chat(request: Request):
+    return templates.TemplateResponse("chat.html", {"request": request})
+
+
+@app.get("/api/current_user")
+def get_user(request: Request):
+    return request.cookies.get("X-Authorization")
+
+
+class RegisterValidator(BaseModel):
+    username: str
+
+    class Config:
+        orm_mode = True
+
+
+@app.post("/api/register")
+def register_user(user: RegisterValidator, response: Response):
+    response.set_cookie(key="X-Authorization", value=user.username, httponly=True)
+
+
+class SocketManager:
+    def __init__(self):
+        self.active_connections: List[(WebSocket, str)] = []
+
+    async def connect(self, websocket: WebSocket, user: str):
+        await websocket.accept()
+        self.active_connections.append((websocket, user))
+
+    def disconnect(self, websocket: WebSocket, user: str):
+        self.active_connections.remove((websocket, user))
+
+    async def broadcast(self, data: dict):
+        for connection in self.active_connections:
+            await connection[0].send_json(data)
+
+
+manager = SocketManager()
+
+
+@app.websocket("/api/chat")
+async def chat(websocket: WebSocket):
+    sender = websocket.cookies.get("X-Authorization")
+    if sender:
+        await manager.connect(websocket, sender)
+        response = {
+            "sender": sender,
+            "message": "got connected"
+        }
+        await manager.broadcast(response)
+        try:
+            while True:
+                data = await websocket.receive_json()
+                await manager.broadcast(data)
+        except WebSocketDisconnect:
+            manager.disconnect(websocket, sender)
+            response['message'] = "left"
+            await manager.broadcast(response)
+
+
+
+# @app.post("/send_message")
+# def addMessage(data: Model):
+    
+#     doc = {
+#         message : data.msg,
+#         created_at : datetime.datetime.utcnow()
+#     }
+    
+#     msg_coll.insert_one(doc)
+    
+#     print('hello')
+#     return 'world'
+
+# @app.get("/getAllMessages")
+# def getAllMessages():
+#     docs = msg_coll.find({})
+#     return docs
 
 
 @app.get("/get_recommendations")
@@ -81,6 +153,3 @@ def get_recommendations(genre, target_key, popularity, tempo):
     # print(d['tracks'][6]['href'])
     print(response.text)
     
-@app.get("/")
-def get_categories():
-    return 'hello world'
